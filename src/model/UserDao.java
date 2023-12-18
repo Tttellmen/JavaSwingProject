ee8ad14d38d0f7f9a7ee434f6e4f2caf47327e39
Tttellmen <90588057+Tttellmen@users.noreply.github.com>
2023-12-18 17:44:26 +0800

    UserDao.java
    .

--- src/model/UserDao.java
+++ src/model/UserDao.java
@@ -3,7 +3,11 @@ package model;
 import java.sql.SQLException;
 import java.util.HashMap;
 
+// Хэрэглэгчийн интерфэйс
 public interface UserDao {
+    // Хэрэглэгчийн мэдээллийг авах функц
     public HashMap<String, User> getDataUsers() throws SQLException;
+
+    // Хэрэглэгч нэмэх функц
     public void addUser(User user) throws SQLException;
 }
