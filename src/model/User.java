8d12735add6eb8a72d8df8ed81b5fff2e643c1e0
Tttellmen <90588057+Tttellmen@users.noreply.github.com>
2023-12-18 17:43:06 +0800

    User.java
    .

--- src/model/User.java
+++ src/model/User.java
@@ -7,15 +7,20 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.HashMap;
 
-public class User{
-    private int ID;
-    private String name;
-    private String mail;
-    private String password;
-    private int type;
-    public User(){
+// Хэрэглэгчийн класс
+public class User {
+    private int ID; // ID
+    private String name; // Нэр
+    private String mail; // И-мэйл хаяг
+    private String password; // Нууц үг
+    private int type; // Төрөл
+
+    // Конструктор
+    public User() {
 
     }
+
+    // Конструктор параметртэй
     public User(String name, String mail, String password, int type) {
         this.name = name;
         this.mail = mail;
@@ -23,40 +28,52 @@ public class User{
         this.type = type;
     }
 
+    // ID унших функц
     public int getID() {
         return ID;
     }
+
+    // Нууц үгийг унших функц
     public String getPassword() {
         return password;
     }
+
+    // Төрөл унших функц
     public int getType() {
         return type;
     }
 
+    // ID тохируулах функц
     public void setID(int ID) {
         this.ID = ID;
     }
 
+    // Нэрийг утгаар солих функц
     public String getName() {
         return name;
     }
 
+    // Нэр тохируулах функц
     public void setName(String name) {
         this.name = name;
     }
 
+    // И-мэйл хаягийг унших функц
     public String getMail() {
         return mail;
     }
 
+    // И-мэйл хаягийг тохируулах функц
     public void setMail(String mail) {
         this.mail = mail;
     }
 
+    // Нууц үгийг тохируулах функц
     public void setPassword(String password) {
         this.password = password;
     }
 
+    // Төрөл тохируулах функц
     public void setType(int type) {
         this.type = type;
     }
