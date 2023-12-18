13c6c82b7a3ec9ff7593d397132008c0e3362b67
Tttellmen <90588057+Tttellmen@users.noreply.github.com>
2023-12-18 17:34:02 +0800

    BookDao.java
    .

--- src/model/BookDao.java
+++ src/model/BookDao.java
@@ -2,6 +2,8 @@ package model;
 
 import java.sql.SQLException;
 
+// BookDao интерфэйс
 public interface BookDao {
+    // Номын мэдээллийг авах гишүүн функц
     public void getDataBooks() throws SQLException;
 }
